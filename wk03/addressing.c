a43ab1b6445f3e4b05ca6c8f2639e8e22c0a9732
Ian Toy <iantoy@uab.edu>
2023-09-07 14:40:29 -0500

    Corrected format specifier errors in addressing.c example

--- wk03/addressing.c
+++ wk03/addressing.c
@@ -9,10 +9,10 @@ int main() {
 
     ptr = &n;
 
-    printf("ptr:\t%x\n", ptr);
-    printf("&n:\t%x\n", &n);
-    printf("*ptr:\t%d\n", *ptr);
-    printf("n:\t%d\n", n);
-    printf("&ptr:\t%x\n", &ptr);
+    printf("ptr:\t%p\n", ptr); // prints the address stored in ptr
+    printf("&n:\t%p\n", &n); // prints the address of n
+    printf("*ptr:\t%d\n", *ptr); // prints the value at the address stored in ptr
+    printf("n:\t%d\n", n); // prints the value of n
+    printf("&ptr:\t%p\n", &ptr); // prints the address OF ptr
 
 }
